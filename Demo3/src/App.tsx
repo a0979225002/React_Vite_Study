381fb2b3cc9a7babd1a310a1eb4da9be4bc3c113
xiao_lipin <a0979225002@gmail.com>
2024-12-26 00:39:19 +0800

    [新增] 動態改變頁面透過 useState 方式重新渲染參數

--- Demo3/src/App.tsx
+++ Demo3/src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState} from "react";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [list, setCount] = useState([1, 2, 3, 4]);
+    return (
+        <div className="container">
+            <ul>
+                {list.map((item, index) => {
+                    return <li key={index}>{item}
+                        <button
+                            style={{marginLeft: "20px"}}
+                            onClick={() => onSetItemCountButtonClick(index, list, setCount)}>
+                            加100
+                        </button>
+                        <button
+                            style={{marginLeft: "20px"}}
+                            onClick={() => onDestoryItemButtonClick(index, list, setCount)}>
+                            刪除此項
+                        </button>
+                    </li>
+                })}
+            </ul>
+            <button onClick={() => onAddCountButtonClick(list, setCount)}>增加</button>
+        </div>
+    )
+}
+function onDestoryItemButtonClick(index: number, list: number[], updateList: (list: number[]) => void) {
+    let newList = [...list];
+    newList.splice(index, 1);
+    updateList(newList);
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function onSetItemCountButtonClick(index: number, list: number[], updateList: (list: number[]) => void) {
+    let newList = [...list];
+    newList[index] = newList[index]+100;
+    updateList(newList);
+}
+
+/**
+ *
+ * @param list
+ * @param updateList
+ */
+function onAddCountButtonClick(list: number[], updateList: (list: number[]) => void) {
+    let newList = [...list];
+    let maxNumber = 0;
+    for (let i = 0; i < newList.length; i++) {
+        let random = Math.floor(Math.random() * (newList.length - 1));
+        const temp = newList[i];
+        newList[i] = newList[random];
+        newList[random] = temp;
+        if(temp > maxNumber) maxNumber = temp;
+    }
+    newList.push(maxNumber + 1);
+    updateList(newList);
 }
 
 export default App
